e493202d0b5a90e4f1ec7f5525053c871ec13289
Andrei Igna <andrei.igna@layered.studio>
2017-09-18 17:27:26 +0200

    Added - option for admins to peak at hidden menu items, new settings page

--- if-menu.php
+++ if-menu.php
@@ -30,23 +30,28 @@ License: GPL2
 class If_Menu {
 
 	public static function init() {
-    global $pagenow;
+		global $pagenow;
 
-    load_plugin_textdomain('if-menu', false, dirname(plugin_basename(__FILE__)) . '/languages');
+		load_plugin_textdomain('if-menu', false, dirname(plugin_basename(__FILE__)) . '/languages');
 
 		if (is_admin()) {
 			add_action('admin_enqueue_scripts', 'If_Menu::admin_init');
 			add_action('wp_update_nav_menu_item', 'If_Menu::wp_update_nav_menu_item', 10, 2);
 			add_filter('wp_edit_nav_menu_walker', 'If_Menu::customWalker'); 
-      add_action('wp_nav_menu_item_custom_fields', 'If_Menu::menu_item_fields');
-      add_action('wp_nav_menu_item_custom_title', 'If_Menu::menu_item_title');
+			add_action('wp_nav_menu_item_custom_fields', 'If_Menu::menu_item_fields');
+			add_action('wp_nav_menu_item_custom_title', 'If_Menu::menu_item_title');
+			add_action('init', 'If_Menu::saveSettings');
+			add_action('admin_menu', function() {
+				add_submenu_page('themes.php', __('If Menu', 'if-menu'), __('If Menu', 'if-menu'), 'edit_theme_options', 'if-menu', 'If_Menu::page');
+			});
 
-      if ($pagenow !== 'nav-menus.php') {
-        add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
-      }
+			if ($pagenow !== 'nav-menus.php') {
+				add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
+			}
 		} else {
-      add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
-    }
+			add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
+			add_action('wp_enqueue_scripts', 'If_Menu::addAssets');
+		}
 	}
 
 	public static function get_conditions( $for_testing = false ) {
@@ -123,20 +128,93 @@ class If_Menu {
 	public static function admin_init() {
 		global $pagenow;
 
-    if ($pagenow == 'nav-menus.php') {
-      wp_enqueue_script('if-menu-js', plugins_url('assets/if-menu.js', __FILE__), array('jquery'));
-      wp_enqueue_style('if-menu-css', plugins_url('assets/if-menu.css', __FILE__));
+		if ($pagenow == 'nav-menus.php' || $pagenow == 'themes.php') {
+		  wp_enqueue_script('if-menu-js', plugins_url('assets/if-menu.js', __FILE__), array('jquery'));
+		  wp_enqueue_style('if-menu-css', plugins_url('assets/if-menu.css', __FILE__));
 
-      wp_localize_script('if-menu-js', 'IfMenu', array(
-        'conflictErrorMessage'  =>  sprintf(
-          wp_kses(
-            __('<strong>If Menu</strong> detected a conflict with another plugin or theme and may not work as expected. <a href="%s" target="_blank">Read more about the issue here</a>', 'if-menu'),
-            array('a' => array('href' => array()), 'strong' => array())
-          ),
-          esc_url('https://wordpress.org/plugins/if-menu/faq/')
-        )
-      ));
-    }
+		  wp_localize_script('if-menu-js', 'IfMenu', array(
+		    'conflictErrorMessage'  =>  sprintf(
+		      wp_kses(
+		        __('<strong>If Menu</strong> detected a conflict with another plugin or theme and may not work as expected. <a href="%s" target="_blank">Read more about the issue here</a>', 'if-menu'),
+		        array('a' => array('href' => array()), 'strong' => array())
+		      ),
+		      esc_url('https://wordpress.org/plugins/if-menu/faq/')
+		    )
+		  ));
+		}
+	}
+
+	public static function saveSettings() {
+		global $pagenow;
+
+		if ($pagenow == 'themes.php' && isset($_POST['if-menu-settings'])) {
+			update_option('if-menu-peak', $_POST['if-menu-peak']);
+		}
+	}
+
+	public static function page() {
+		$ifMenuPeak = get_option('if-menu-peak');
+		?>
+
+		<div class="wrap about-wrap if-menu-wrap">
+			<h1><?php _e('If Menu', 'if-menu') ?></h1>
+			<p class="about-text">
+				<?php _e('Thanks for using <strong>If Menu</strong>! This plugin allows you to hide or only show menu items with visibility rules, ex: <code>Display menu item if User is logged in</code> or <code>Hide menu item if Using mobile device</code>', 'if-menu') ?>
+				—
+				<a href="<?php echo admin_url('nav-menus.php') ?>" class="button"><?php _e('Manage your menus', 'if-menu') ?></a></p>
+			<hr class="wp-header-end">
+
+			<div class="feature-section two-col">
+				<div class="col">
+					<h3><?php _e('Basic set of visibility rules', 'if-menu') ?></h3>
+					<ul>
+						<li><?php _e('User state: User is logged in', 'if-menu') ?></li>
+						<li><?php _e('User roles: Admin, Editor, Author, etc', 'if-menu') ?></li>
+						<li><?php _e('Page type: Front page, Single page, Single post', 'if-menu') ?></li>
+						<li><?php _e('Device Is Mobile', 'if-menu') ?></li>
+						<li><?php _e('Language Is RTL', 'if-menu') ?></li>
+					</ul>
+					<p><?php _e('Theme / plugin developers can extend the plugin by adding custom visibility rules', 'if-menu') ?></p>
+				</div>
+				<div class="col">
+					<h3><?php _e('Visibility rule type', 'if-menu') ?></h3>
+					<p><?php _e('Visibity rules can have 2 states:', 'if-menu') ?></p>
+					<ul>
+						<li><strong class="if-menu-green"><?php _e('Show', 'if-menu') ?></strong> - <?php _e('show menu item only if vsibility rule passes', 'if-menu') ?></li>
+						<li><strong class="if-menu-red"><?php _e('Hide', 'if-menu') ?></strong> - <?php _e('hide menu item if rule passes', 'if-menu') ?></li>
+					</ul>
+				</div>
+			</div>
+
+			<hr>
+
+			<h3 class="title"><?php _e('Settings', 'if-menu') ?></h3>
+
+			<form method="post" action="">
+				<table class="form-table">
+					<tbody>
+						<tr>
+							<th scope="row"><?php _e('If Menu peak', 'if-menu') ?></th>
+							<td>
+								<fieldset>
+									<label><input type="checkbox" name="if-menu-peak" value="1" <?php checked($ifMenuPeak, 1) ?>> <?php _e('Enable If Menu peak', 'if-menu') ?></label><br>
+								</fieldset>
+								<p class="description"><?php _e('Let administrators preview hidden menu items on website', 'if-menu') ?></p>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p class="submit"><input type="submit" name="if-menu-settings" id="submit" class="button button-primary" value="<?php _e('Save Changes', 'if-menu') ?>"></p>
+			</form>
+
+		</div>
+
+		<?php
+	}
+
+	public static function addAssets() {
+		wp_enqueue_style('if-menu-site-css', plugins_url('assets/if-menu-site.css', __FILE__));
 	}
 
   public static function menu_item_fields( $item_id ) {
@@ -182,6 +260,7 @@ class If_Menu {
               </optgroup>
             <?php endforeach ?>
           </select>
+          </span>
           <select class="menu-item-if-menu-enable-next" name="menu-item-if-menu-enable[<?php echo esc_attr( $item_id ); ?>][]">
             <option value="false">+</option>
             <option value="and" <?php if (isset($if_menu_enable[$index + 1])) selected( 'and', $if_menu_enable[$index + 1] ) ?>><?php esc_html_e( 'AND', 'if-menu' ) ?></option>
